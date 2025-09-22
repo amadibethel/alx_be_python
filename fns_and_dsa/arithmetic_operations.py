3b0926897eec6d7f4c78283195d2e5428545fbcd
Bethel Amadi <amadibethel@icloud.com>
2025-09-22 14:10:50 +0200

    Add  Arithmetic Operations Function file to Project

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,6 @@
 # fns_and_dsa/arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
-    """
-    Perform basic arithmetic operations on two numbers.
-
-    Parameters:
-        num1 (float): The first number.
-        num2 (float): The second number.
-        operation (str): The operation to perform. 
-                         Options: 'add', 'subtract', 'multiply', 'divide'.
-
-    Returns:
-        float | str: The result of the operation, or an error message if invalid.
-    """
+def perform_operation(num1, num2, operation):
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
